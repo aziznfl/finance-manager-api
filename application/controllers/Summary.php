7d34c3ca954d7bba7f48925b05c9e179f65702cc
Aziz Nurfalah <aziz.nurfalah@bukalapak.com>
2021-04-14 08:39:58 +0700

    fix cors

--- application/controllers/Summary.php
+++ application/controllers/Summary.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Summary extends MY_Controller {
 
     function __construct() {
+		header('Access-Control-Allow-Origin: *');
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         parent::__construct();
 		$this->load->model('M_Transaction');
 
